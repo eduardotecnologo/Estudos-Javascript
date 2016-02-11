d756d7b53ca5c173f0f5742a7902e4e89ed71260
eduardotecnologo <eduardotecnologo@hotmail.com>
2016-02-10 22:26:14 -0200

    Estdos Sobre React

---
+++ React/CatchOfTheDay/scripts/main.js
@@ -0,0 +1,14 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+/*
+StorePicker
+This will tel us make <StorePicker/>
+ */
+var StorePicker = React.createClass({
+     render : function(){
+        return(
+         <p>Hello</p>
+         )
+     }
+});
+ReactDOM.render(<StorePicker/>, document.querySelector('#main'));
