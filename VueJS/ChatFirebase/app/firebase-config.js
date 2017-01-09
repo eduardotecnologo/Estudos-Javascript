a7c36f593f6f641720389e816b1d9b74a66de108
eduardodeveloper <eduardotecnologo@hotmail.com>
2017-01-09 18:52:29 -0200

    Chat com FireBase e Vuejs

---
+++ VueJS/ChatFirebase/app/firebase-config.js
@@ -0,0 +1,11 @@
+define('firebase-config', function(){
+	// Initialize Firebase
+	return {
+		aapiKey: "DADOS DE CONEXAO AQUI",
+		authDomain: "DADOS DE CONEXAO AQUI",
+		databaseURL: "DADOS DE CONEXAO AQUI",
+		storageBucket: "DADOS DE CONEXAO AQUI",
+		messagingSenderId: "DADOS DE CONEXAO AQUI"
+
+	};
+});
