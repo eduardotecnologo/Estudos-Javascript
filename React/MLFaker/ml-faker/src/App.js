877160b27803d27f492e7ac62006d2246977c67b
eduardodeveloper <eduardotecnologo@hotmail.com>
2017-11-11 15:08:20 -0200

    Connect FireBase

--- React/MLFaker/ml-faker/src/App.js
+++ React/MLFaker/ml-faker/src/App.js
@@ -1,24 +1,32 @@
 import React, { Component } from 'react'
 import logo from './logo.svg'
-
 import HeaderHome from './HeaderHome'
 import AnuncioHome from './AnuncioHome'
 import FooterHome from './FooterHome'
-import LinkCategorias from './LinkCategorias'
+import LinkCategoria from './LinkCategoria'
 import base from './base'
 
 class App extends Component {
   constructor(props){
     super(props)
     this.state = {
-      categorias:[]
+      categorias:[],
+      anuncios:[]
     }
-    base.baseToState('categorias',{
+    base.bindToState('categorias',{
       context: this,
       state: 'categorias'
     })
+    base.bindToState('anuncios', {
+      context: this,
+      state: 'anuncios',
+      queries:{
+        limitToLast: 3
+      }
+    })
   }
   render() {
+    let index = 0
     return (
       <div className="App">
         <HeaderHome />
@@ -26,21 +34,27 @@ class App extends Component {
           <div className="container">
           <h3>Últimos Anúncios</h3>
             <div class="row">
-              <AnuncioHome />
-              <AnuncioHome />
-              <AnuncioHome />
+              {this.state.anuncios.map((anuncio, indice) => {
+                return <AnuncioHome key={indice} anuncio={anuncio} />
+                
+              })}
             </div>
           <h3>Categorias</h3>
-          <div class="row">
-              <LinkCategorias categoria={{ categoria: 'Bicicleta', 'icon': 'fa-lightbulb-o' }} />
-              <LinkCategorias categoria={{ categoria: 'Bicicleta', 'icon': 'fa-lightbulb-o' }} />
-              <LinkCategorias categoria={{ categoria: 'Bicicleta', 'icon': 'fa-lightbulb-o' }} />
+          {/* Testando
+          {JSON.stringify(this.state.categorias)} */}
+          <div className="row">
+              { this.state.categorias.map((cat, indice) => {
+                return [
+                  <LinkCategoria categoria={cat} key={indice}/>,
+                  ++index%4 === 0 && <div key={'c'+indice} className="w-100"></div>
+                  ]
+              })}
           </div>
         </div>
         <FooterHome />
         </div>
       </div>
-    );
+    )
   }
 }
 
