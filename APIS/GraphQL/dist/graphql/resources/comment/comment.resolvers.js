4c46099cf458d6dcc750711359de8b0d8c45484b
eduardotecnologo <eduardotecnologo@hotmail.com>
2018-01-14 23:28:16 -0200

    create all Resolvers

---
+++ APIS/GraphQL/dist/graphql/resources/comment/comment.resolvers.js
@@ -0,0 +1,57 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.commentResolvers = {
+    Comment: {
+        user: (comment, args, { db }, info) => {
+            return db.User
+                .findById(comment.get('user'));
+        }
+    },
+    post: (comment, args, { db }, info) => {
+        return db.Post
+            .findById(comment.get('post'));
+    },
+    Query: {
+        commentsByPost: (parent, { postId, first = 10, offset = 0 }, { db }, info) => {
+            return db.Comment
+                .findAll({
+                where: { post: postId },
+                limit: first,
+                offset: offset
+            });
+        }
+    },
+    Mutation: {
+        createComment: (parent, { input }, { db }, info) => {
+            return db.sequelize.transaction((t) => {
+                return db.Comment
+                    .create(input, { transaction: t });
+            });
+        },
+        updateComment: (parent, { id, input }, { db }, info) => {
+            id = parseInt(id);
+            return db.sequelize.transaction((t) => {
+                return db.Comment
+                    .findById(id)
+                    .then((comment) => {
+                    if (!comment)
+                        throw new Error('Comment not found!');
+                    return comment.update(input, { transaction: t });
+                });
+            });
+        },
+        deleteComment: (parent, { id }, { db }, info) => {
+            id = parseInt(id);
+            return db.sequelize.transaction((t) => {
+                return db.Comment
+                    .findById(id)
+                    .then((comment) => {
+                    if (!comment)
+                        throw new Error('Comment not found!');
+                    return comment.destroy({ transaction: t })
+                        .then(comment => !!comment);
+                });
+            });
+        }
+    }
+};
--- APIS/GraphQL/dist/graphql/resources/user/user.resolvers.js
+++ APIS/GraphQL/dist/graphql/resources/user/user.resolvers.js
@@ -1,6 +1,16 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.userResolvers = {
+    User: {
+        posts: (user, { first = 10, offset = 0 }, { db }, info) => {
+            return db.Post
+                .findAll({
+                where: { author: user.get('id') },
+                limit: first,
+                offset: offset
+            });
+        }
+    },
     Query: {
         users: (parent, { first = 10, offset = 0 }, { db }, info) => {
             return db.User.findAll({
@@ -15,6 +25,52 @@ exports.userResolvers = {
                 if (!user)
                     throw new Error(`User with id ${id} not found!`);
                 return user;
+            });
+        }
+    },
+    Mutation: {
+        createUser: (parent, { input }, { db }, info) => {
+            return db.sequelize.transaction((t) => {
+                return db.User
+                    .create(input, { transaction: t });
+            });
+        },
+        updateUser: (parent, { id, input }, { db }, info) => {
+            id = parseInt(id);
+            return db.sequelize.transaction((t) => {
+                return db.User
+                    .findById(id)
+                    .then((user) => {
+                    if (!user)
+                        throw new Error(`User with id ${id} not found!`);
+                    return user.update(input, { transaction: t });
+                });
+            });
+        },
+        updateUserPassword: (parent, { id, input }, { db }, info) => {
+            id = parseInt(id);
+            return db.sequelize.transaction((t) => {
+                return db.User
+                    .findById(id)
+                    .then((user) => {
+                    if (!user)
+                        throw new Error(`User with id ${id} not found!`);
+                    return user.update(input, { transaction: t })
+                        .then((user) => !!user);
+                });
+            });
+        },
+        deleteUser: (parent, { id }, { db }, info) => {
+            id = parseInt(id);
+            return db.sequelize.transaction((t) => {
+                return db.User
+                    .findById(id)
+                    .then((user) => {
+                    if (!user)
+                        throw new Error(`User with id ${id} not found!`);
+                    return user.destroy({ transaction: t })
+                        .then(user => !!user);
+                });
             });
         }
     }
