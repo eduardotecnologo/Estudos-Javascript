aee0d6e1fd884cec28c1701d9350b7fb87f9b6e8
eduardotecnologo <eduardotecnologo.com>
2018-10-12 20:10:52 -0300

    Create Project Edu séries

---
+++ React/my-series/src/App.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react'
+
+
+class App extends Component {
+  componentDidMount(){
+    
+  }
+  render() {
+    return (
+      <div>
+  <nav className="navbar navbar-default navbar-fixed-top" role="navigation">
+    <div className="container">
+      <div className="navbar-header page-scroll">
+        <a className="navbar-brand page-scroll" href="#page-top">
+            <img src="images/logo.png" height="30" />
+        </a>
+      </div>
+
+      <div className="collapse navbar-collapse navbar-ex1-collapse">
+        <ul className="nav navbar-nav">
+          <li>
+            <a href="">Menu item</a>
+          </li>
+        </ul>
+      </div>
+
+    </div>
+  </nav>
+
+
+  <section id="intro" className="intro-section">
+    <div className="container">
+      <div className="row">
+        <div className="col-lg-12">
+          <h1><img src="images/logo.png"  height="100" /></h1>
+          <p>Nunca mais esqueça uma série que você assistiu ou que alguém lhe indicou.</p>
+        </div>
+      </div>
+    </div>
+  </section>
+  </div>
+    )
+  }
+}
+
+export default App
