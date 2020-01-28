7ea8bfa6053d035f680e7e12cc111906e033404b
eduardotecnologo <eduardotecnologo@hotmail.com>
2020-01-28 17:20:11 -0300

    CSS Modules

---
+++ React2020/Ionic4ReactApollo/IonicreactSpaceX/src/components/LaunchesItem.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Launch } from '../generated/graphql'
+import { IonCard, IonImg } from '@ionic/react'
+import styles from './LaunchesItem.module.scss'
+
+interface Props {
+  launch: Launch
+}
+const LaunchesItem: React.FC<Props> = props => {
+  const { launch } = props
+  return (
+    <IonCard button className={styles.card}>
+      <IonImg src={launch.links.flickr_images[0]} className={styles.img} />
+      <h2 className={styles.cardTitle}>{launch.mission_name}</h2>
+      <p className={styles.cardSubtitle}>{launch.rocket.rocket_name}</p>
+    </IonCard>
+  )
+}
+
+export default LaunchesItem
