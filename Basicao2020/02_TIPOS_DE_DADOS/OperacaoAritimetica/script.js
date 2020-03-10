6a06a28a9fb617794d8a64aa9b39e8357f98d425
eduardotecnologo <eduardotecnologo@hotmail.com>
2020-03-10 14:15:49 -0300

    02_TIPOS_DE_DADOS

---
+++ Basicao2020/02_TIPOS_DE_DADOS/OperacaoAritimetica/script.js
@@ -0,0 +1,6 @@
+console.log(typeof (2 + 5));
+console.log(5 - 13);
+console.log(30 / 3);
+console.log(7 * 30);
+console.log(typeof (20 % 2));
+console.log(21 % 2);
---
+++ Basicao2020/02_TIPOS_DE_DADOS/script.js
@@ -0,0 +1,4 @@
+console.log(typeof 5);
+console.log(typeof 15.5);
+console.log(typeof -20);
+console.log(typeof 30);
