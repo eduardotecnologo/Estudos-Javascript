690f30f80f8a260f7f3ac03975171da0811fd7ef
eduardotecnologo <eduardotecnologo@hotmail.com>
2020-03-11 17:14:44 -0300

    Criando a rota para adicionar registros

--- SearchJob/app.js
+++ SearchJob/app.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const app = express();
 const db = require('./db/connect');
+const bodyParser = require('body-parser');
 
 const PORT = 3000;
 
 app.listen(PORT, function() {
   console.log(`Server running port ${PORT}`);
 });
+
+// Body parser
+app.use(bodyParser.urlencoded({ extended: false }));
+
 //DB Connect
 db.authenticate()
   .then(() => {
@@ -15,7 +20,11 @@ db.authenticate()
   .catch(err => {
     console.log('Error connecting', err);
   });
+
 //Routes
 app.get('/', (req, res) => {
   res.send('App runnig');
 });
+
+//Routes Jobs
+app.use('/jobs', require('./routes/jobs'));
