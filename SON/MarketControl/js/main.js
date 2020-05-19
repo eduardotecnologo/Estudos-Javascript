0e9d1d1a2cc8280018df79444344ac75261390ca
eduardotecnologo <eduardotecnologo@hotmail.com>
2020-05-19 16:13:23 -0300

    Listando dados

--- SON/MarketControl/js/main.js
+++ SON/MarketControl/js/main.js
@@ -11,4 +11,14 @@ function getTotal(list) {
   }
   return total;
 }
+function setList(list) {
+  let table = '<thead class="thead-light"><tr><td>Descrição</td><td>Quantidade</td><td>Valor</td><td>Ação</td></tr></thead><tbody>'
+  for (let key in list) {
+    table += ' <tr><td>' + list[key].desc + '</td><td>' + list[key].amount + '</td><td>' + list[key].value + '</td><td>Editar | Apagar</td></tr>'
+  }
+  table += '</tbody>';
+  document.getElementById("listTable").innerHTML = table;
+}
+
+setList(list);
 console.log(getTotal(list));
