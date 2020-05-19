dc594bf9bc2558057e0c15d0a2a1d2a4303eeb75
eduardotecnologo <eduardotecnologo@hotmail.com>
2020-05-19 15:11:39 -0300

    Configurando a aplicação

---
+++ SON/MarketControl/js/config.js
@@ -0,0 +1,8 @@
+function setConfig() {
+  let texts = {
+    "title": "App Controle de Compras"
+  };
+  document.title = texts.title;
+  document.getElementById("navTitle").innerHTML = texts.title;
+}
+setConfig();
