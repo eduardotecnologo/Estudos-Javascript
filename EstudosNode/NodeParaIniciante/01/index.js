aa54a9f83ca30679b57c1292d3aa255993601ba3
eduardotecnologo <eduardotecnologo@hotmail.com>
2020-06-12 21:18:36 -0300

    Reforçando estudos em NodeJS

---
+++ EstudosNode/NodeParaIniciante/01/index.js
@@ -0,0 +1,15 @@
+/*
+  0 Obter um usuário
+  1 Obter o numero de telefone de usuário a partir de seu ID
+  2 Obter o endereço do usuário pelo ID
+*/
+
+function getUser() {
+
+}
+function getPhone() {
+
+}
+function getAddres() {
+
+}
